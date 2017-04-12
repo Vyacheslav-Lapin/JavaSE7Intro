49cb6f879af610c5bf98128649d08fd7af40d532
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-04-12 09:37:36 +0300

    get/set

--- src/MessageGen.java
+++ src/MessageGen.java
@@ -2,4 +2,23 @@ public class MessageGen {
     public String getMessage() {
         return "Hello, World!!!";
     }
+
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setAge(50);
+
+        Person person2 = new Person();
+        person2.setAge(40);
+
+        Person person3 = new Person();
+        person3.setAge(5);
+
+        Person person4 = new Person();
+        person4.setAge(25);
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+    }
 }
--- src/Person.java
+++ src/Person.java
@@ -6,7 +6,15 @@ public class Person {
         return age;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Person().getAge());
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
