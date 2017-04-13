42b29f7745ba4562b2f9815cbec3980c76e9918c
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2017-04-13 12:22:48 +0300

    lombok

--- src/main/java/com/luxoft/tc/java/basics/bank/Account.java
+++ src/main/java/com/luxoft/tc/java/basics/bank/Account.java
@@ -1,63 +1,21 @@
 package com.luxoft.tc.java.basics.bank;
 
 import com.luxoft.tc.java.basics.Person;
+import lombok.*;
 
-import java.util.Objects;
-
+@AllArgsConstructor
+@Data
 public class Account {
 
     private final Person person;
-
     private int bill;
-
     private int overdraft;
 
-    public Account(Person person, int bill, int overdraft) {
-        this.person = person;
-        this.bill = bill;
-        this.overdraft = overdraft;
-    }
+    public static void main(String[] args) {
 
-    public int getBill() {
-        return bill;
-    }
-
-    public void setBill(int bill) {
-        this.bill = bill;
-    }
-
-    public Person getPerson() {
-        return person;
-    }
-
-    public int getOverdraft() {
-        return overdraft;
-    }
-
-    public void setOverdraft(int overdraft) {
-        // TODO: 12/04/2017 проверки сделать
-        this.overdraft = overdraft;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return Objects.equals(person, account.person);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(person);
-    }
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "person=" + person +
-                ", bill=" + bill +
-                ", overdraft=" + overdraft +
-                '}';
+        Account account = new Account(
+                new Person("John Smith", 55),
+                500,
+                100);
     }
 }
